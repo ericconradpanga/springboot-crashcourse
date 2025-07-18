d93c6d358e4dca9126879f2191311c8ed5ff051e
eric+virgil <eric+virgil@gcash.com>
2025-07-18 11:36:13 +0800

    added remaining tests and code for addition operation

--- session2/calculator-tdd-style/src/main/java/org/example/Calculator.java
+++ session2/calculator-tdd-style/src/main/java/org/example/Calculator.java
@@ -4,4 +4,20 @@ public class Calculator {
     public int sum(int num1, int num2) {
         return num1 + num2;
     }
+
+    public long sum(long num1, long num2) {
+        return num1 + num2;
+    }
+
+    public short sum(short num1, short num2) {
+        return (short) (num1 + num2);
+    }
+
+    public float sum(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public double sum(double num1, double num2) {
+        return num1 + num2;
+    }
 }
--- session2/calculator-tdd-style/src/test/java/TestCalculator.java
+++ session2/calculator-tdd-style/src/test/java/TestCalculator.java
@@ -6,9 +6,33 @@ public class TestCalculator {
     Calculator calculator = new Calculator();
 
     @Test
-    void sumTest(){
+    void sumIntTest(){
         int sum = calculator.sum(1,5);
         assertEquals(6,sum);
+    }
+
+    @Test
+    void sumLongTest(){
+        long sum = calculator.sum(-1,5);
+        assertEquals(4,sum);
+    }
+
+    @Test
+    void sumShortTest(){
+        short sum = (short) calculator.sum(2,-4);
+        assertEquals(-2,sum);
+    }
+
+    @Test
+    void sumFloatTest(){
+        float sum = calculator.sum(3f,7f);
+        assertEquals(10,sum,.001);
+    }
+
+    @Test
+    void sumDoubleTest(){
+        double sum = calculator.sum(6.0,2.0);
+        assertEquals(8.0,sum);
     }
 
 }
